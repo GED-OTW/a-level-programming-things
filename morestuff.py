7af125ed894c1960aec82d443ee6f595b48d972c
giovanni Adjovi <20AdjoviG@cfbs.islington.sch.uk>
2025-10-23 14:15:03 +0100

    updated comments

--- morestuff.py
+++ morestuff.py
@@ -11,7 +11,7 @@ dt = 0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
-    # poll for events
+    # poll for events - comment
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
